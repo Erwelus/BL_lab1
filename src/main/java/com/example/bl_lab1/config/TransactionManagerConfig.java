e11c4ce17a930e2dd2304331476d74cc39321504
EgorMIt <egor.mitrofanov@hotmail.com>
2022-03-25 00:26:03 +0300

    Транзакции

--- src/main/java/com/example/bl_lab1/config/TransactionManagerConfig.java
+++ src/main/java/com/example/bl_lab1/config/TransactionManagerConfig.java
@@ -2,16 +2,37 @@ package com.example.bl_lab1.config;
 
 import com.atomikos.icatch.jta.UserTransactionManager;
 import com.atomikos.icatch.jta.UserTransactionImp;
+import org.springframework.boot.jta.atomikos.AtomikosDataSourceBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
+import org.postgresql.xa.PGXADataSource;
 
 import javax.transaction.SystemException;
 
 @Configuration
 @EnableTransactionManagement
 public class TransactionManagerConfig {
+
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public AtomikosDataSourceBean myDataSource() {
+        PGXADataSource pgxaDataSource = new PGXADataSource();
+        pgxaDataSource.setUrl("jdbc:postgresql://localhost:5432/studs");
+        pgxaDataSource.setPassword("nvn024");
+        pgxaDataSource.setUser("s285583");
+
+        AtomikosDataSourceBean dataSource = new AtomikosDataSourceBean();
+        dataSource.setXaDataSource(pgxaDataSource);
+        // Configure database holding order data
+        return dataSource;
+    }
+
+    @Bean
+    public UserTransactionImp myTransactionImp() {
+        return new UserTransactionImp();
+    }
+
     @Bean(initMethod = "init", destroyMethod = "close")
     public UserTransactionManager userTransactionManager() throws SystemException {
         UserTransactionManager userTransactionManager = new UserTransactionManager();
